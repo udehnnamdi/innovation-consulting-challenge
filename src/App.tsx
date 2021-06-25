80108a837bae7822c875a50a27c6ea646b55c023
udehnnamdi <udehnnamdi.edu@gmail.com>
2021-06-25 21:49:05 +0200

    updated solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { Fragment } from 'react';
-import classes from './components/Layout/HeaderButton.module.css'
 import Header from './components/Layout/Header';
 import SectionOne from './components/Layout/SectionOne';
 import SectionTwo from './components/Layout/SectionTwo';
@@ -9,23 +8,34 @@ import SectionFour from './components/Layout/SectionFour';
 import FooterButton from './components/Layout/FooterButton'
 import {useState, useEffect} from 'react';
 
+
+
 function App() {
-  const [isScrolling, setIsScrolling] = useState(false);
+ 
+const [isScrolling, setIsScrolling] = useState(false);
+const [imgVisible, setImageVisible] = useState(true);
+let runtime:any = null
+
+const onScrollHandler =()=>{
+
+if(window.scrollY>=750){
+  setImageVisible(false)
+}else{
+  setImageVisible(true)
+}
+ setIsScrolling(true)
+
+  clearTimeout(runtime)
+
+  runtime = setTimeout(()=>{
+    setIsScrolling(false)
+  },200)
+
+  
+}
   
 
-  const onScrollHandler = () =>{
-   
-    if(window.scrollY>=750){
-      setIsScrolling(true)
-    }else{
-      setIsScrolling(false)
-    }
-
-}
-
-
-
-    window.addEventListener('scroll', onScrollHandler)
+window.addEventListener('scroll', onScrollHandler)
   
 
   return (
@@ -35,7 +45,7 @@ function App() {
         <SectionTwo/>
         <SectionThree/>
         <SectionFour/>
-       {isScrolling&&<FooterButton/>}
+       {!imgVisible && !isScrolling&&<FooterButton />}
     </Fragment>
   );
 }
